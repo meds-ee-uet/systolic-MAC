009acea32f4de77e5cfb48012809f240e6dcaeea
abdmz162 <abdmz.uetian23@gmail.com>
2025-07-02 16:30:06 +0500

    added edge cases, identified the valid signal stable time limit

--- i_mac_sv/mac_int_fsm_tb.sv
+++ i_mac_sv/mac_int_fsm_tb.sv
@@ -41,44 +41,74 @@ initial begin
     beta=16'd40;
     vld=1;//will last for one whole clock cycle
     #5 vld=0;
-    #30
+    #50
     alpha=16'd10;
     beta=16'd16;
     vld=1;
     #5 vld=0;
-    #30 alpha=16'd50;
+    #50 alpha=16'd50;
     beta=16'd25;
     vld=1;
     #5 vld=0;
-    #30 alpha=16'd100;
+    #50 alpha=16'd100;
     beta=16'd23;
     vld=1;
     #5 vld=0;
-    #30 alpha=16'd100;
+    #50 alpha=16'd100;
     beta=16'd24;
     vld=1;
     #5 vld=0;
 
     //negative case tests:
-    #30 alpha=16'd100;
+    #50 alpha=16'd100;
     beta=-16'd2;
     vld=1;
     #5 vld=0;
-    #30 alpha=16'd11;
+    #50 alpha=16'd11;
     beta=-16'd11;
     vld=1;
     #5 vld=0;
-    #30 alpha=16'd7;
+    #50 alpha=16'd7;
     beta=16'd2;
     vld=1;
     #5 vld=0;
-    #30 alpha=16'd40;
+    #50 alpha=16'd40;
     beta=-16'd50;
     vld=1;
     #5 vld=0;
-    #30 alpha=-16'd111;
+    #50 alpha=-16'd111;
     beta=-16'd2;
     vld=1;
+
+    //edge cases tests(overflow  )
+    #5 vld=0;
+    #50 alpha=16'd32767;
+    beta=16'd32767;
+    vld=1;
+    #5 vld=0;
+    #50 alpha=-16'd32768;
+    beta=-16'd32768;
+    vld=1;
+
+    #5 vld=0;
+    #50 alpha=-16'd32768;
+    beta=16'd32767;
+    vld=1;
+    #5 vld=0;
+    #50 alpha=16'd32767;
+    beta=-16'd32768;
+    vld=1;
+
+    #5 vld=0;
+    #50 alpha=16'd32767;
+    beta=16'd32767;
+    vld=1;
+    #5 vld=0;
+    #50 alpha=16'd32767;
+    beta=16'd32767;
+    vld=1;
+
+
     #5 vld=0;
     #50 rst=1;
     #5 rst=0;
