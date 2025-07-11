325f0e57c7e17e38aac11f07e2ab087a5f97445f
abdmz162 <abdmz.uetian23@gmail.com>
2025-07-11 11:44:32 +0500

    modified and corrected counter code

--- rtl/counter.sv
+++ rtl/counter.sv
@@ -8,20 +8,26 @@ module counter(
 
     always_ff @(posedge clk or posedge reset) begin
         if (reset) begin
-            en_y <= 1'b0;
             count <= 3'b0;
         end else if (done) begin
             if (count == 3'b111) begin
-                en_y <= 1'b1;
                 count <= 3'b0;
             end else begin
-                en_y <= 1'b0;
                 count <= count + 3'b1;
             end
         end else begin
-            en_y <= 1'b0;
+            count <= count;
         end
     end
+    
+    always_comb begin
+        if (reset) begin
+            en_y = 1'b0; // Disable output when reset
+        end else begin
+            en_y = (count == 3'b111) ? 1'b1 : 1'b0; // Enable when count reaches 7
+        end
+    end
+
 
 endmodule
 
